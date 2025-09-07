8b8c27883979636ef0856f20dccd43287fff96ea
Sri Harsha Mutakodoor <sriharsha.mutakodoor@gmail.com>
2025-09-07 15:10:16 +0530

    add pagination to the app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React, { useEffect, useState } from "react";
 const App=()=>{
 
     const [products,setProducts]= useState([]);
+    const [page,setPage]=useState(1);
 
     const fetchProduct=async()=>{
-        const data = await fetch("https://dummyjson.com/products");
+        const data = await fetch("https://dummyjson.com/products?limit=100");
         const json = await data.json();
       
 
@@ -21,19 +22,36 @@ const App=()=>{
 
     return (
       <div>
-        {products.length > 0 && <div className="products my-8">
-            {products.map((item,index)=>{
+        {products.length > 0 && <div className="products border border-black flex flex-wrap  m-8">
+            {products.slice(page*10 -10,page*10).map((item,index)=>{
                 return (
-                <span key={item.id} className='flex flex-col items-center' >
-                    <img  src={item.thumbnail} alt={item.title}/>
+                <span key={item.id} className='flex flex-col items-center w-40' >
+                    <img  src={item.thumbnail} alt={item.title}/> 
                     <span>{item.title}</span>
                 </span>
                 )
             })}
             
             </div>}
+
+
+            { products.length > 0 && <div className='flex justify-center'>
+                <span>◀️</span>
+
+                {
+                    [...Array(products.length/10)].map((_,i)=> {
+                        return <button onClick={()=> setPage(i)}
+                        className="mx-4 w-6 border border-black" key={i}>{i+1}</button>
+                    })
+                }
+
+                
+                
+                
+                </div>}
             
         </div>
+
     )
 }
 
